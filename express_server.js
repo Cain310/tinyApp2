157a2f08ad9d1fef6c92e7ae7c511c17fe400269
Michael Cain <michaelcaincb@gmail.com>
2019-04-10 21:05:13 +0000

    add edit delete functionality about to implement cookies.

--- express_server.js
+++ express_server.js
@@ -7,66 +7,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-
 var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
 function generateRandomString() {
-    var text = "";
-    var possible =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 7; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
+  for (var i = 0; i < 7; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+  res.send("Hello! This should be your home page but it is not...");
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+  res.render("urls_new");
 });
 
+// app.get("/urls/update", (req, res) => {
+//   res.render("urls_show");
+// });
+
 app.get("/urls/:shortURL", (req, res) => {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-    const longURL = urlDatabase[req.params.shortURL];
-    console.log('this one', longURL);
-    res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.post("/urls", (req, res) => {
-    const i = generateRandomString()
-    // console.log('this one', req.body);
-    urlDatabase[i] = req.body.longURL;
-    res.redirect("/urls");
+  const i = generateRandomString();
+  urlDatabase[i] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/Delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/", (req, res) => {
+  // const userId = urlDatabase[req.body.shortUrl].userId;
+  // console.log(urlDatabase);
+  // console.log(urlDatabase[req.params.shortUrl]);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  console.log[req.params.shortURL];
+
+  res.redirect("/urls/");
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
