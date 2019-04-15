7785d0379f9ceada38b194752b3cb186d43b2a5a
Michael Cain <michaelcaincb@gmail.com>
2019-04-15 16:42:58 +0000

    fixed registration bug in ejs <form> tag, removed PLease login message on registration page, switched var to es6, comented code

--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
 
 app.use(express.static("public"));
+
 app.use(
   cookieSession({
     name: "session",
@@ -17,11 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+// Hardcoded url database
 const urlDatabase = {
   b2xVn2: { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
   lsm5xK: { longURL: "http://www.google.com", userID: "user2RandomID" }
 };
 
+//Hardcoded user database
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -34,21 +37,24 @@ const users = {
     password: genHashPassword("dishwasher-funk")
   }
 };
+//Helper functions here
+//generates a hashed password using bcrypt
 function genHashPassword(value) {
   return (hashedPassword = bcrypt.hashSync(value, 10));
 }
 
+//Generates a random string of 6 letters and numbers
 function generateRandomString() {
-  var text = "";
-  var possible =
+  let text = "";
+  let possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 7; i++) {
+  for (let i = 0; i < 7; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
+//Retrieves user info to cross refernce against input login registration user data.
 function retrieveUser(email, password) {
   for (username in users) {
     let userEmail = users[username]["email"];
@@ -63,6 +69,7 @@ function retrieveUser(email, password) {
   return false;
 }
 
+//Function that makes sure the user is logged in to view urls on main urls page.
 function userUrls(userId) {
   let urls = {};
   for (shorturl in urlDatabase) {
@@ -73,10 +80,13 @@ function userUrls(userId) {
   return urls;
 }
 
+
+//All GET routes here
 app.get("/", (req, res) => {
   res.send("Hello! This should be your home page but it is not...");
 });
 
+//Main urls page where only logged in user can create edit or delete urls.
 app.get("/urls", (req, res) => {
   let templateVars = {
     users: users[req.session["userId"]],
@@ -85,21 +95,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//Page where logged in registered user can create or add new urls to there profile.
 app.get("/urls/new", (req, res) => {
   let templateVars = { users: users[req.session["userId"]] };
   res.render("urls_new", templateVars);
 });
 
+//register page users who are not already registered can register.
 app.get("/register", (req, res) => {
-  let templateVars = { users: users[req.session["userId"]] };
+  let templateVars = { users: users[req.session["userId"]], showLogin: false };
   res.render("urls_register", templateVars);
 });
 
+//Login page where only registered users can login
 app.get("/login", (req, res) => {
   let templateVars = { urls: urlDatabase, users: users[req.session["userId"]] };
   res.render("urls_login", templateVars);
 });
 
+//short urls verification page.
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -109,11 +123,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
+//All POST routes here
 app.post("/urls", (req, res) => {
   const i = generateRandomString();
   urlDatabase[i] = {
@@ -123,13 +139,14 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls");
 });
 
+//register page where the magic happens encrypts passwords and randomizes id's, pushes input object to fake database
+//error messages and redirect when succesful.
 app.post("/register", (req, res) => {
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  const i = generateRandomString();
+  const randomID = generateRandomString();
   const { email, password } = req.body;
-
   const newUser = {
-    id: i,
+    id: randomID,
     email,
     password: hashedPassword
   };
@@ -137,7 +154,7 @@ app.post("/register", (req, res) => {
   if (retrieveUser(email, password)) {
     res.status(400).send("You are already a registered user");
   } else if (!email || !password) {
-    res.redirect("/register");
+    res.send("UH OH Please try Again!");
   } else {
     users[i] = newUser;
     req.session.userId = i;
@@ -145,6 +162,7 @@ app.post("/register", (req, res) => {
   }
 });
 
+//Login page retrieve users who have already registered using the helper function up top. Error messages if not already registered.
 app.post("/login", (req, res) => {
   const user = retrieveUser(req.body.email, req.body.password);
   if (user) {
@@ -155,11 +173,13 @@ app.post("/login", (req, res) => {
   }
 });
 
+//Logout button application, redirects to login and deletes session encrypted cookie.
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/login");
 });
 
+//Delete urls that only belong to specific longed in user. cookies verify users.
 app.post("/urls/:shortURL/delete", (req, res) => {
   const userId = urlDatabase[req.params.shortURL].userID;
   if (userId === req.session["userId"]) {
@@ -170,9 +190,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
+//working short url link to long url.
 app.post("/urls/:shortURL/", (req, res) => {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL;
-
   res.redirect("/urls/");
 });
 
